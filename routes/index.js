a780459cc05b4e22814bc1ebd6bd4f9958bcb830
likesoomti <soomti@likelion.org>
2018-02-24 09:28:04 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,13 @@ var router = express.Router();
 const api = require('../api');
 
 /* GET home page. */
-router.get('/keyboard', function(req, res, next) {
-  res.send(api.test());
+router.get('/keyboard', (req, res) => {
+  const menu = {
+      type: 'buttons',
+      buttons: ["메뉴1", "메뉴2", "메뉴3"]
+  };
+  res.set({
+      'content-type': 'application/json'
+  }).send(JSON.stringify(menu));
 });
-
 module.exports = router;
